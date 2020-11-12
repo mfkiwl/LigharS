9659dd49e7ae6c94a94de47b501d99f9b770097b
PENGUINLIONG <admin@penguinliong.moe>
2020-11-12 22:10:33 +0800

    Formatted trignometric function impls

--- src/dummy.cpp
+++ src/dummy.cpp
@@ -6,29 +6,36 @@ void L_x() {
 
 // approximates sin of x degree in the range from 0 to 180.
 float BhaskaraSin(float x) {
-	if(x >= 0)
-		return 4 * x * (180 - x) / (40500 - x * (180 - x));
-	else
-		return -4 * x * (180 - x) / (40500 - x * (180 - x));
+    if(x >= 0)
+        return 4 * x * (180 - x) / (40500 - x * (180 - x));
+    else
+        return -4 * x * (180 - x) / (40500 - x * (180 - x));
 }
 
 
 // using taylor series
 float TaylorSin(float x) {
-	return 1.0f * x - x * x * x / 6 + x * x * x * x * x / 120;
+    float x2 = x * x;
+    float x3 = x2 * x;
+    float x5 = x3 * x2;
+    return 1.0f * x - x3 / 6 + x5 / 120;
 }
 float TaylorCos(float x) {
-	return 1.0f - x * x / 4 + x * x * x * x / 24 - x * x * x * x * x * x / 720 + x * x * x * x * x * x * x * x / 40320;
+    float x2 = x * x;
+    float x4 = x2 * x2;
+    float x6 = x4 * x2;
+    float x8 = x4 * x4;
+    return 1.0f - x2 / 4 + x4 / 24 - x6 / 720 + x8 / 40320;
 }
 
 
 
 
-float CarmackSqrt(float x) { 
-	float xhalf = 0.5f * x;   
-	int i = *(int*)&x;    
-	i = 0x5f3759df - (i >> 1);   
-	x = *(float*)&i;    
-	x = x * (1.5f - xhalf * x * x);    
-	return 1/x; 
+float CarmackSqrt(float x) {
+    float xhalf = 0.5f * x;
+    int i = *(int*)&x;
+    i = 0x5f3759df - (i >> 1);
+    x = *(float*)&i;
+    x = x * (1.5f - xhalf * x * x);
+    return 1/x;
 }
