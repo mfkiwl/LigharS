4514e1243a02f021509a258c7e892df7889d48cf
PENGUINLIONG <admin@penguinliong.moe>
2020-11-12 22:10:46 +0800

    Structurized ray tracing

--- include/geom.hpp
+++ include/geom.hpp
@@ -1,5 +1,6 @@
 #include <cmath>
 #include <cassert>
+#include <algorithm>
 
 namespace gpumagi {
 
@@ -86,6 +87,123 @@ inline Triangle make_tri(const Point& a, const Point& b, const Point& c) {
 
 
 
+struct TransformRow {
+  float x, y, z, w;
+};
+inline float dot(const TransformRow& a, const Vector& b) {
+  return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+inline float dot(const TransformRow& a, const Point& b) {
+  return a.x * b.x + a.y * b.y + a.z * b.z + a.w;
+}
+
+struct Transform {
+  union {
+    float mat[12];
+    struct { TransformRow r1, r2, r3; };
+    TransformRow rows[3];
+  };
+
+  Transform() : mat{ 1,0,0,0, 0,1,0,0, 0,0,1,0 } {}
+  Transform(TransformRow r1, TransformRow r2, TransformRow r3) : r1(r1), r2(r2), r3(r3) {}
+  Transform(float a, float b, float c, float d,
+            float e, float f, float g, float h,
+            float i, float j, float k, float l) : mat{ a,b,c,d,e,f,g,h,i,j,k,l } {}
+  Transform(float mat[12]) { mat = mat; }
+  Transform(const Transform&) = default;
+  Transform(Transform&&) = default;
+  Transform& operator=(const Transform&) = default;
+  Transform& operator=(Transform&&) = default;
+
+  inline Transform operator*(const Transform& rhs) const {
+    Vector c1 { rhs.mat[0], rhs.mat[4], rhs.mat[8] };
+    Vector c2 { rhs.mat[1], rhs.mat[5], rhs.mat[9] };
+    Vector c3 { rhs.mat[2], rhs.mat[6], rhs.mat[10] };
+    Point c4 { rhs.mat[3], rhs.mat[7], rhs.mat[11] };
+    return Transform{
+      dot(r1, c1), dot(r1, c2), dot(r1, c3), dot(r1, c4),
+      dot(r2, c1), dot(r2, c2), dot(r2, c3), dot(r2, c4),
+      dot(r3, c1), dot(r3, c2), dot(r3, c3), dot(r3, c4),
+    };
+  }
+  inline Vector apply_vec(const Vector& rhs) const {
+    return Vector {
+      dot(rows[0], rhs),
+      dot(rows[1], rhs),
+      dot(rows[2], rhs),
+    };
+  }
+  inline Point apply_pt(const Point& rhs) const {
+    return Point {
+      dot(rows[0], rhs),
+      dot(rows[1], rhs),
+      dot(rows[2], rhs)
+    };
+  }
+
+  inline Transform scale(float x, float y, float z) const {
+    return Transform { x,0,0,0, 0,y,0,0, 0,0,z,0 } * (*this);
+  }
+  inline Transform scale(Vector v) const {
+    return scale(v.x, v.y, v.z);
+  }
+  inline Transform translate(float x, float y, float z) const {
+    return Transform { 1,0,0,x, 0,1,0,y, 0,0,1,z } * (*this);
+  }
+  inline Transform translate(Vector v) const {
+    return translate(v.x, v.y, v.z);
+  }
+  inline Transform rotate(float x, float y, float z, float rad) const {
+    float sin = std::sinf(rad);
+    float cos = std::cosf(rad);
+    float rcos = 1.0f - cos;
+    return Transform {
+        cos + rcos * x * x, rcos * x * y - sin * z, rcos * x * z + sin * y, 0,
+        rcos * y * x + sin * z, cos + rcos * y * y, rcos * y * z - sin * x, 0,
+        rcos * z * x - sin * y, rcos * z * y + sin * x, cos + rcos * z * z, 0,
+    } *(*this);
+  }
+  inline Transform rotate(Vector axis, float rad) const {
+    return rotate(axis.x, axis.y, axis.z, rad);
+  }
+  inline Transform rotate_vec2vec(Vector from, Vector to) const {
+    auto axis = normalize(cross(from, to));
+    auto rad = std::acos(dot(from, to));
+    return rotate(axis, rad);
+  }
+  inline Transform inverse() const {
+    float det {
+      r1.x * (r2.y * r3.z - r2.z * r3.y) -
+      r2.x * (r1.y * r3.z - r1.z * r3.y) +
+      r3.x * (r1.y * r2.z - r1.z * r2.y)
+    };
+    TransformRow r1_ {
+      (r2.y * r3.z - r3.y * r2.z) / det,
+      (r1.z * r3.y - r1.y * r3.z) / det,
+      (r1.y * r2.z - r1.z * r2.y) / det,
+      (-r1.y * r2.z * r3.w - r1.z * r2.w * r3.y - r1.w * r2.y * r3.z +
+        r1.w * r2.z * r3.y + r1.z * r2.y * r3.w + r1.y * r2.w * r3.z) / det,
+    };
+    TransformRow r2_ {
+      (r2.z * r3.x - r2.x * r3.z) / det,
+      (r1.x * r3.z - r1.z * r3.x) / det,
+      (r2.x * r1.z - r1.x * r2.z) / det,
+      (r1.x * r2.z * r3.w + r1.z * r2.w * r3.x + r1.w * r2.x * r3.z -
+        r1.w * r2.z * r3.x - r1.z * r2.x * r3.w - r1.x * r2.w * r3.z) / det,
+    };
+    TransformRow r3_ {
+      (r2.x * r3.y - r2.y * r3.x) / det,
+      (r1.y * r3.x - r1.x * r3.y) / det,
+      (r1.x * r2.y - r2.x * r1.y) / det,
+      (-r1.x * r2.y * r3.w - r1.y * r2.w * r3.x - r1.w * r2.x * r3.y +
+        r1.w * r2.y * r3.x + r1.y * r2.x * r3.w + r1.x * r2.w * r3.y) / det,
+    };
+    return Transform { r1_, r2_, r3_ };
+  }
+};
+
+
+
 struct Barycentric {
   f32 u, v;
 };
@@ -102,12 +220,15 @@ Barycentric make_bary(const Point& p_, const Triangle& tri) {
   f32 denom = d00 * d11 - d01 * d01;
   f32 u = (d11 * d20 - d01 * d21) / denom;
   f32 v = (d00 * d21 - d01 * d20) / denom;
+
+  u = std::min(1.0f, std::max(0.0f, u));
+  v = std::min(1.0f, std::max(0.0f, v));
+
   return Barycentric { u, v };
 }
 inline bool is_valid_bary(const Barycentric& bary) {
-  return bary.u >= 0 || bary.v >= 0 && bary.u + bary.v <= 1;
+  return bary.u >= 0 && bary.v >= 0 && bary.u + bary.v <= 1;
 }
-
 
 
 enum HitKind {
@@ -115,19 +236,33 @@ enum HitKind {
   L_HIT_KIND_FRONT = 1,
   L_HIT_KIND_BACK  = 2,
 };
+struct TriangleHit {
+  Barycentric bary;
+  HitKind kind;
+  f32 t;
+};
 // Returns positive ray time if the ray intersects the triangle plane at front;
 // negative ray time if the ray intersects the triangle plane at back; NaN if
 // the ray doesn't intersect the triangle.
-f32 ray_cast_tri(const Ray& ray, const Triangle& tri) {
+bool ray_cast_tri(const Ray& ray, const Triangle& tri, TriangleHit& hit) {
   Vector n = normalize(cross(tri.u, tri.v));
   // Distance from the ray origin to the triangle plane.
   f32 r1 = dot(tri.o - ray.o, n);
   // Length of projection of the ray direction vector in normal direction.
   f32 r2 = dot(ray.v, n);
-  if (r1 * r2 <= 0.0) { return NAN; }
+  if (r1 * r2 <= 0.0) { return false; }
   // Distance from the ray origin to the triangle.
   f32 t = r1 / r2;
-  return r2 < 0.0 ? t : -t;
+
+  Point p = ray.o + ray.v * t;
+  Barycentric bary = make_bary(p, tri);
+  if (!is_valid_bary(bary)) { return false; }
+  HitKind kind = r2 < 0.0 ? L_HIT_KIND_FRONT : L_HIT_KIND_BACK;
+
+  hit.bary = bary;
+  hit.kind = kind;
+  hit.t = t;
+  return true;
 }
 
 
--- tests/test.cpp
+++ tests/test.cpp
@@ -4,35 +4,170 @@
 
 using namespace gpumagi;
 
-struct Intersection {
-  Barycentric bary;
-  HitKind kind;
-  Point p;
-  f32 t;
-  u32 itri;
+struct Traversable {
+  std::vector<Triangle> tris;
 };
 
-bool traverse(const Ray& ray, const std::vector<Triangle>& tris, Intersection& inters) {
-  for (size_t i = 0; i < tris.size(); ++i) {
-    const auto& tri = tris[i];
-    f32 t = ray_cast_tri(ray, tri);
-    if (std::isnan(t)) { continue; }
+typedef void (*Shader)();
 
-    HitKind kind = t < 0 ? L_HIT_KIND_BACK : L_HIT_KIND_FRONT;
-    t = abs(t);
-    Point p = ray.o + ray.v * t;
-    Barycentric bary = make_bary(p, tri);
-    if (bary.u < 0 || bary.v < 0 || bary.u + bary.v > 1) { continue; }
+struct Pipeline {
+  Shader* shaders;
+  u32 rg_offset;
+  u32 ch_offset;
+  u32 ms_offset;
+};
+struct ShaderBindingTable {
+  const void** datas;
+};
 
-    inters.bary = bary;
-    inters.kind = kind;
-    inters.p = p;
-    inters.t = t;
-    inters.itri = i;
-    return true;
+struct LaunchDim {
+  u32 x, y, z;
+};
+
+enum ShaderStage {
+  L_SHADER_STAGE_RAY_GENERATION,
+  L_SHADER_STAGE_CLOSEST_HIT,
+  L_SHADER_STAGE_MISS,
+};
+
+
+
+struct TraversalPassContext {
+  // Set during draw calls.
+  const Pipeline* pipe;
+  const ShaderBindingTable* sbt;
+  LaunchDim launch_size;
+};
+struct TraversalThreadContext {
+  // Set by scheduler during thread launch.
+  LaunchDim launch_id;
+  ShaderStage stage; // Current execution stage.
+};
+struct TraversalTraceContext {
+  // Set by shader during trace calls.
+  const Traversable* trav;
+  Ray ray;
+  void* payload;
+
+  // Set by the ray-tracing accelerator during trace calls.
+  TriangleHit hit;
+  u32 itri;
+  u32 ich_data;
+  u32 ims_data;
+};
+
+
+
+//
+// Execution contexts. (Currently global variables.)
+static       TraversalPassContext   pass_ctxt;
+thread_local TraversalThreadContext thread_ctxt;
+thread_local TraversalTraceContext  trace_ctxt; // Only used during trace calls.
+
+
+
+//
+// Device-side procedures.
+
+// Trace a ray in `trav`.
+void trace(const Traversable* trav, const Ray& ray, f32 tmin, f32 tmax, u32 ich, u32 ims, void* payload) {
+  trace_ctxt.trav = trav;
+  trace_ctxt.ray = ray;
+  trace_ctxt.payload = payload;
+  trace_ctxt.ray = ray;
+  for (size_t i = 0; i < trav->tris.size(); ++i) {
+    TriangleHit hit;
+    if (ray_cast_tri(ray, trav->tris[i], hit) && tmin <= hit.t && hit.t < tmax) {
+      trace_ctxt.hit = hit;
+      trace_ctxt.itri = i;
+      // Successful hit an triangle within t-range.
+      thread_ctxt.stage = L_SHADER_STAGE_MISS;
+      pass_ctxt.pipe->shaders[pass_ctxt.pipe->ch_offset + ich]();
+      thread_ctxt.stage = L_SHADER_STAGE_RAY_GENERATION;
+    }
   }
-  return false;
+  // Failed to hit any triangle.
+  thread_ctxt.stage = L_SHADER_STAGE_MISS;
+  pass_ctxt.pipe->shaders[pass_ctxt.pipe->ms_offset + ims]();
+  thread_ctxt.stage = L_SHADER_STAGE_RAY_GENERATION;
 }
+
+const void* get_sbt_data() {
+  switch (thread_ctxt.stage) {
+  case L_SHADER_STAGE_RAY_GENERATION:
+    return pass_ctxt.sbt->datas[pass_ctxt.pipe->rg_offset];
+  case L_SHADER_STAGE_CLOSEST_HIT:
+    return pass_ctxt.sbt->datas[pass_ctxt.pipe->ch_offset + trace_ctxt.ich_data];
+  case L_SHADER_STAGE_MISS:
+    return pass_ctxt.sbt->datas[pass_ctxt.pipe->ms_offset + trace_ctxt.ims_data];
+  }
+}
+
+const Traversable& get_traversable()   { return *trace_ctxt.trav; }
+const Ray&         get_ray()           { return trace_ctxt.ray; }
+f32                get_ray_tmin()      { return trace_ctxt.hit.t; }
+HitKind            get_hit_kind()      { return trace_ctxt.hit.kind; }
+Barycentric        get_bary()          { return trace_ctxt.hit.bary; }
+u32                get_itri()          { return trace_ctxt.itri; }
+void*              get_payload()       { return trace_ctxt.payload; }
+const LaunchDim&   get_launch_size()   { return pass_ctxt.launch_size; }
+const LaunchDim&   get_launch_id()     { return thread_ctxt.launch_id; }
+bool               is_front_face_hit() { return trace_ctxt.hit.kind == L_HIT_KIND_FRONT; }
+bool               is_back_face_hit()  { return trace_ctxt.hit.kind == L_HIT_KIND_BACK; }
+
+
+
+
+//
+// Host-side procedures.
+
+void launch(const Pipeline& pipe, const ShaderBindingTable& sbt, LaunchDim launch_size) {
+  pass_ctxt.pipe = &pipe;
+  pass_ctxt.sbt = &sbt;
+  pass_ctxt.launch_size = launch_size;
+  for (auto k = 0; k < launch_size.z; ++k) {
+    thread_ctxt.launch_id.z = k;
+    for (auto j = 0; j < launch_size.y; ++j) {
+      thread_ctxt.launch_id.y = j;
+      for (auto i = 0; i < launch_size.x; ++i) {
+        thread_ctxt.launch_id.x = i;
+        pipe.shaders[pipe.rg_offset]();
+      }
+    }
+  }
+}
+
+
+
+const int FRAMEBUF_EDGE = 4;
+
+
+
+struct RGData {
+  const Traversable* trav;
+  std::vector<uint8_t>* framebuf;
+};
+void raygen() {
+  const auto& data = *(const RGData*)get_sbt_data();
+
+  LaunchDim launch_size = get_launch_size();
+  LaunchDim launch_id = get_launch_id();
+  Ray ray = make_ray(
+    make_pt(float(launch_id.x) / FRAMEBUF_EDGE, float(launch_id.y) / FRAMEBUF_EDGE, 1),
+    make_vec(0, 0, -1)
+  );
+
+  bool hit;
+  trace(data.trav, ray, 1e-3, 1e5, 0, 0, &hit);
+  (*data.framebuf)[launch_id.x + launch_id.y * launch_size.x] = hit;
+}
+void closest_hit() {
+  *(bool*)get_payload() = true;
+}
+void miss() {
+  *(bool*)get_payload() = false;
+}
+
 
 int main(int argc, const char** argv) {
   // Test your code here for now. We will have to write a lot of tests so you
@@ -45,24 +180,32 @@ int main(int argc, const char** argv) {
       make_pt(0, 1, 0)
     ),
   };
+  Traversable trav { tris };
 
+  const int FRAMEBUF_EDGE = 4;
   std::vector<uint8_t> framebuf;
-  framebuf.resize(64 * 64);
+  framebuf.resize(FRAMEBUF_EDGE * FRAMEBUF_EDGE);
 
-  Intersection inters;
+  Shader shaders[3] = { raygen, closest_hit, miss };
+  Pipeline pipe;
+  pipe.shaders = shaders;
+  pipe.rg_offset = 0;
+  pipe.ch_offset = 1;
+  pipe.ms_offset = 2;
 
-  for (auto i = 0; i < 64; ++i) {
-    for (auto j = 0; j < 64; ++j) {
-      auto ray = make_ray(
-        make_pt(float(i) / 64, float(j) / 64, 1),
-        make_vec(0, 0, -1)
-      );
-      if (traverse(ray, tris, inters)) {
-        framebuf[i * 64 + j] = 1;
-        std::cout << 1;
-      } else {
-        std::cout << 0;
-      }
+
+  RGData rg_data {};
+  rg_data.trav = &trav;
+  rg_data.framebuf = &framebuf;
+  const void* sbt_datas[3] = { &rg_data, nullptr, nullptr };
+  ShaderBindingTable sbt;
+  sbt.datas = sbt_datas;
+
+  launch(pipe, sbt, { FRAMEBUF_EDGE, FRAMEBUF_EDGE });
+
+  for (auto j = 0; j < FRAMEBUF_EDGE; ++j) {
+    for (auto i = 0; i < FRAMEBUF_EDGE; ++i) {
+      std::cout << (int)framebuf[i + j * FRAMEBUF_EDGE] << " ";
     }
     std::cout << std::endl;
   }
